78fb14de8d4ab3ad320f17ef5f5ac0e45ddae265
Saeid Gholami <saeidgholami101@gmail.com>
2020-11-03 22:27:02 +0330

    post model

--- project_blog/blog/models.py
+++ project_blog/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_date = models.DateTimeField(auto_now=True)
+    last_edit = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
